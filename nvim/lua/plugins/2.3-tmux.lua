914989bdfaf9826706550a91440130b8bed00d0b
Michal Späte <michal.spaete@vorwerk.de>
2024-07-15 00:43:27 +0200

    added tmux navigator

---
+++ nvim/lua/plugins/2.3-tmux.lua
@@ -0,0 +1,9 @@
+retrun {
+    {
+        "christoomey/vim-tmux-navigator",
+        vim.keymap.set('n', 'C-h', ':TmuxNavigator<CR>'),
+        vim.keymap.set('n', 'C-j', ':TmuxNavigator<CR>'),
+        vim.keymap.set('n', 'C-k', ':TmuxNavigator<CR>'),
+        vim.keymap.set('n', 'C-l', ':TmuxNavigator<CR>'),
+    }
+}
