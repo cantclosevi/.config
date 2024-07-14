2a99bdfd4751f80591e96b01d3ceb2cad9c1ec7e
Michal Späte <michal.spaete@vorwerk.de>
2024-07-14 17:43:24 +0200

    added setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Paths
+
+
+echo "Cloning .dotfiles directory"
+echo "Creating .config directory"
+echo "mkdir .config"
+
+
+echo "git clone https://github.com/cantclosevi/.dotfiles"
+
+echo "Linking configuration files"
+
