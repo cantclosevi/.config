65d8f09bd56ccd0c46910d450b6a20d693385871
Michal Späte <michal.spaete@vorwerk.de>
2024-07-14 21:12:30 +0200

    added neo-tree

---
+++ nvim/lua/plugins/00.dependencies.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"nvim-tree/nvim-web-devicons"
+	},
+    {
+        "nvim-lua/plenary.nvim"
+    },
+    {
+        "MunifTanjim/nui.nvim"
+    }
--- nvim/lua/plugins/02.statusbar.lua
+++ nvim/lua/plugins/02.statusbar.lua
@@ -1,8 +1,5 @@
 return {
 	{
-		"nvim-tree/nvim-web-devicons"
-	},
-	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
 		config = function()
